857997483b834f71d6f7de14967f1bb18a85b4b2
JordLoop <Jordloop@gmail.com>
2017-08-10 15:57:54 -0700

    add dynamic details page for gummis

--- Controllers/GummiController.cs
+++ Controllers/GummiController.cs
@@ -8,10 +8,16 @@ namespace GummiBearKing.Controllers
     public class GummiController : Controller
     {
         private GummiDbContext db = new GummiDbContext();
-
+        //View All
         public IActionResult Index()
         {
             return View(db.Gummis.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisGummi = db.Gummis.FirstOrDefault(gummis => gummis.GummiId == id);
+            return View(thisGummi);
         }
     }
 }
