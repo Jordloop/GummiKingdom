ac4beecd17a1f4066b043b6616d4d010806ffa6b
JordLoop <Jordloop@gmail.com>
2017-08-10 15:46:05 -0700

    add ability to view all gummis in db

---
+++ Controllers/GummiController.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using GummiBearKing.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GummiBearKing.Controllers
+{
+    public class GummiController : Controller
+    {
+        private GummiDbContext db = new GummiDbContext();
+
+        public IActionResult Index()
+        {
+            return View(db.Gummis.ToList());
+        }
+    }
+}
